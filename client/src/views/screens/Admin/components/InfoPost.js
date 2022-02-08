8b9b5ba7e403ab6a5ba67e3c7a8cf7cad305618e
devdkhanh <duykhanh06.work@gmail.com>
2022-02-08 20:27:01 +0700

    fix admin

--- client/src/views/screens/Admin/components/InfoPost.js
+++ client/src/views/screens/Admin/components/InfoPost.js
@@ -11,145 +11,145 @@ import ButtonToggle from '../../../components/ButtonToggle';
 import Dialog from '../../../components/Dialog';
 
 function InfoPost({ props, onDeletePost }) {
-	const { newCancelToken } = useCancelToken();
-	const [toggle, setToggle] = useState(props.isDelete);
-	const [category, setCategory] = useState({});
-	const [user, setUser] = useState({});
-	const [post, setPost] = useState({});
-	const [showDialog, setShowDialog] = useState(false);
+    const { newCancelToken } = useCancelToken();
+    const [toggle, setToggle] = useState(props.isDelete);
+    const [category, setCategory] = useState({});
+    const [user, setUser] = useState({});
+    const [post, setPost] = useState({});
+    const [showDialog, setShowDialog] = useState(false);
 
-	useEffect(() => {
-		setPost(props);
-	}, [props]);
+    useEffect(() => {
+        setPost(props);
+    }, [props]);
 
-	useEffect(() => {
-		if (props.category) {
-			setCategory(
-				...listCategory.filter(item => item.value === props.category),
-			);
-		}
-		return () => setCategory({});
-	}, [props.category]);
+    useEffect(() => {
+        if (props.category) {
+            setCategory(
+                ...listCategory.filter((item) => item.value === props.category)
+            );
+        }
+        return () => setCategory({});
+    }, [props.category]);
 
-	/********** get user info from id **********/
-	useEffect(() => {
-		if (props.idUser) {
-			(async () => {
-				try {
-					/********** call api **********/
-					const res = await usersAPI.getContact(
-						props.idUser,
-						newCancelToken(),
-					);
-					if (res.status === 1) {
-						setUser(res.data);
-					}
-				} catch (err) {}
-			})();
-		}
+    /********** get user info from id **********/
+    useEffect(() => {
+        if (props.idUser) {
+            (async () => {
+                try {
+                    /********** call api **********/
+                    const res = await usersAPI.getContact(
+                        props.idUser,
+                        newCancelToken()
+                    );
+                    if (res.status === 1) {
+                        setUser(res.data);
+                    }
+                } catch (err) {}
+            })();
+        }
 
-		return () => {
-			setUser({});
-		};
-	}, [props.idUser, newCancelToken]);
+        return () => {
+            setUser({});
+        };
+    }, [props.idUser, newCancelToken]);
 
-	const handleActivePost = useCallback(() => {
-		(async () => {
-			try {
-				/********** call api **********/
-				const data = {
-					id: props._id,
-					toggle: !toggle,
-				};
-				const res = await adminAPI.updateActivePost(
-					data,
-					newCancelToken(),
-				);
-				if (res) {
-					toast.info('Đã cập nhật bài viết');
-					setPost(res);
-					setToggle(!toggle);
-				} else {
-					toast.error(
-						'Không thể cập nhật trạng thái cho bài viết này',
-					);
-				}
-			} catch (err) {}
-		})();
-	}, [props._id, newCancelToken, toggle, setToggle, setPost]);
+    const handleActivePost = useCallback(() => {
+        (async () => {
+            try {
+                /********** call api **********/
+                const data = {
+                    id: props._id,
+                    toggle: !toggle,
+                };
+                const res = await adminAPI.updateActivePost(
+                    data,
+                    newCancelToken()
+                );
+                if (res) {
+                    toast.info('Đã cập nhật bài viết');
+                    setPost(res);
+                    setToggle(!toggle);
+                } else {
+                    toast.error(
+                        'Không thể cập nhật trạng thái cho bài viết này'
+                    );
+                }
+            } catch (err) {}
+        })();
+    }, [props._id, newCancelToken, toggle, setToggle, setPost]);
 
-	const handleDelete = useCallback(() => {
-		(async () => {
-			try {
-				/********** call api **********/
-				const res = await adminAPI.deletePost(
-					props._id,
-					newCancelToken(),
-				);
-				if (res) {
-					toast.info('Đã xóa bài viết');
-					onDeletePost(res);
-				} else {
-					toast.error(
-						'Không thể cập nhật trạng thái cho bài viết này',
-					);
-				}
-			} catch (err) {}
-		})();
-	}, [onDeletePost, newCancelToken, props._id]);
+    const handleDelete = useCallback(() => {
+        (async () => {
+            try {
+                /********** call api **********/
+                const res = await adminAPI.deletePost(
+                    props._id,
+                    newCancelToken()
+                );
+                if (res) {
+                    toast.info('Đã xóa bài viết');
+                    onDeletePost(res);
+                } else {
+                    toast.error(
+                        'Không thể cập nhật trạng thái cho bài viết này'
+                    );
+                }
+            } catch (err) {}
+        })();
+    }, [onDeletePost, newCancelToken, props._id]);
 
-	const classTag = useMemo(() => {
-		if (!post.isReady) return { class: 'not-ready', text: 'Chưa duyệt' };
-		if (post.isDelete) return { class: 'delete', text: 'Đã ẩn' };
-		if (post.status === 0)
-			return { class: 'active', text: 'Đang sẵn sàng' };
-		else return { class: 'un-active', text: 'Đã cho mượn' };
-	}, [post.isDelete, post.status, post.isReady]);
+    const classTag = useMemo(() => {
+        if (!post.isReady) return { class: 'not-ready', text: 'Chưa duyệt' };
+        if (post.isDelete) return { class: 'delete', text: 'Đã ẩn' };
+        if (post.status === 0)
+            return { class: 'active', text: 'Đang sẵn sàng' };
+        else return { class: 'un-active', text: 'Đã cho mượn' };
+    }, [post.isDelete, post.status, post.isReady]);
 
-	return (
-		<>
-			<Dialog
-				active={showDialog}
-				txtBtnSubmit="Xóa bài viết"
-				title="Bạn chắc chắn muốn xóa bài viết này?"
-				styleDialog="danger"
-				onSubmit={handleDelete}
-				onClose={() => setShowDialog(false)}
-			/>
-			<tr>
-				<td>
-					<AvatarImg avatar={user.avatar} />
-				</td>
-				<td>{user.lastName + ' ' + user.firstName}</td>
-				<td>
-					<NavLink to={`/post/${post.slug}`}>{post.title}</NavLink>
-				</td>
-				<td>{category.text}</td>
-				<td>{post.author}</td>
-				<td>
-					<span className={`tag ${classTag.class}`}>
-						{classTag.text}
-					</span>
-				</td>
-				<td>
-					<span>
-						{new Date(post.createdAt).toLocaleDateString('en-GB')}
-					</span>
-				</td>
-				<td>
-					<ButtonToggle toggle={!toggle} onClick={handleActivePost} />
-				</td>
-				<td>
-					<button
-						className="btn--delete"
-						onClick={() => setShowDialog(true)}
-					>
-						Xóa bài đăng
-					</button>
-				</td>
-			</tr>
-		</>
-	);
+    return (
+        <>
+            <Dialog
+                active={showDialog}
+                txtBtnSubmit="Xóa bài viết"
+                title="Bạn chắc chắn muốn xóa bài viết này?"
+                styleDialog="danger"
+                onSubmit={handleDelete}
+                onClose={() => setShowDialog(false)}
+            />
+            <tr>
+                <td>
+                    <AvatarImg avatar={user.avatar} />
+                </td>
+                <td>{user.lastName + ' ' + user.firstName}</td>
+                <td>
+                    <NavLink to={`/post/${post.slug}`}>{post.title}</NavLink>
+                </td>
+                <td>{category?.text}</td>
+                <td>{post?.author}</td>
+                <td>
+                    <span className={`tag ${classTag.class}`}>
+                        {classTag.text}
+                    </span>
+                </td>
+                <td>
+                    <span>
+                        {new Date(post.createdAt).toLocaleDateString('en-GB')}
+                    </span>
+                </td>
+                <td>
+                    <ButtonToggle toggle={!toggle} onClick={handleActivePost} />
+                </td>
+                <td>
+                    <button
+                        className="btn--delete"
+                        onClick={() => setShowDialog(true)}
+                    >
+                        Xóa bài đăng
+                    </button>
+                </td>
+            </tr>
+        </>
+    );
 }
 
 export default memo(InfoPost);
--- client/src/views/screens/Admin/components/InfoPostCensorship.js
+++ client/src/views/screens/Admin/components/InfoPostCensorship.js
@@ -14,161 +14,161 @@ import listCategory from '../../../../constant/listCategory';
 import Dialog from '../../../components/Dialog';
 
 function InfoPost({ props, onSetPosts }) {
-	const socket = useContext(SocketContext);
-	const { newCancelToken } = useCancelToken();
-	const [category, setCategory] = useState({});
-	const [user, setUser] = useState({});
-	const [message, setMessage] = useState('');
-	const [showForm, setShowForm] = useState(false);
-	const [showDialog, setShowDialog] = useState(false);
+    const socket = useContext(SocketContext);
+    const { newCancelToken } = useCancelToken();
+    const [category, setCategory] = useState({});
+    const [user, setUser] = useState({});
+    const [message, setMessage] = useState('');
+    const [showForm, setShowForm] = useState(false);
+    const [showDialog, setShowDialog] = useState(false);
 
-	useEffect(() => {
-		if (props.category) {
-			setCategory(
-				...listCategory.filter(item => item.value === props.category),
-			);
-		}
-		return () => setCategory({});
-	}, [props.category]);
+    useEffect(() => {
+        if (props.category) {
+            setCategory(
+                ...listCategory.filter((item) => item.value === props.category)
+            );
+        }
+        return () => setCategory({});
+    }, [props.category]);
 
-	/********** get user info from id **********/
-	useEffect(() => {
-		if (props.idUser) {
-			(async () => {
-				try {
-					/********** call api **********/
-					const res = await usersAPI.getContact(
-						props.idUser,
-						newCancelToken(),
-					);
-					if (res.status === 1) {
-						setUser(res.data);
-					}
-				} catch (err) {}
-			})();
-		}
+    /********** get user info from id **********/
+    useEffect(() => {
+        if (props.idUser) {
+            (async () => {
+                try {
+                    /********** call api **********/
+                    const res = await usersAPI.getContact(
+                        props.idUser,
+                        newCancelToken()
+                    );
+                    if (res.status === 1) {
+                        setUser(res.data);
+                    }
+                } catch (err) {}
+            })();
+        }
 
-		return () => {
-			setUser({});
-		};
-	}, [props.idUser, newCancelToken]);
+        return () => {
+            setUser({});
+        };
+    }, [props.idUser, newCancelToken]);
 
-	/********** update State msg when input onChange **********/
-	const handleChange = e => {
-		const content = e.target.value;
-		setMessage(content.trim());
-	};
+    /********** update State msg when input onChange **********/
+    const handleChange = (e) => {
+        const content = e.target.value;
+        setMessage(content.trim());
+    };
 
-	const handleAccept = async () => {
-		try {
-			/********** call api **********/
-			const res = await adminAPI.accpetPost(
-				{ id: props._id },
-				newCancelToken(),
-			);
-			if (res) {
-				onSetPosts(prev => prev.filter(post => post._id !== res));
-				toast.success('Đã chấp nhận bài viết');
-				socket.emit('message:accpectPost', {
-					idUser: props.idUser,
-					slug: props.slug,
-					title: props.title,
-				});
-			} else {
-				toast.error('Không thể cập nhật trạng thái cho bài viết này');
-			}
-		} catch (err) {}
-	};
+    const handleAccept = async () => {
+        try {
+            /********** call api **********/
+            const res = await adminAPI.accpetPost(
+                { id: props._id },
+                newCancelToken()
+            );
+            if (res) {
+                onSetPosts((prev) => prev.filter((post) => post._id !== res));
+                toast.success('Đã chấp nhận bài viết');
+                socket.emit('message:accpectPost', {
+                    idUser: props.idUser,
+                    slug: props.slug,
+                    title: props.title,
+                });
+            } else {
+                toast.error('Không thể cập nhật trạng thái cho bài viết này');
+            }
+        } catch (err) {}
+    };
 
-	const handleNotAccept = async () => {
-		if (trimSpaces(message).trim() !== '') {
-			socket.emit('message:notAccpectPost', {
-				idUser: props.idUser,
-				slug: props.slug,
-				title: props.title,
-				content: trimSpaces(message),
-			});
-			setShowForm(false);
-			onSetPosts(prev => prev.filter(post => post._id !== props._id));
-		} else {
-			toast.warn('Nhập lý do từ chối');
-		}
-	};
+    const handleNotAccept = async () => {
+        if (trimSpaces(message).trim() !== '') {
+            socket.emit('message:notAccpectPost', {
+                idUser: props.idUser,
+                slug: props.slug,
+                title: props.title,
+                content: trimSpaces(message),
+            });
+            setShowForm(false);
+            onSetPosts((prev) => prev.filter((post) => post._id !== props._id));
+        } else {
+            toast.warn('Nhập lý do từ chối');
+        }
+    };
 
-	return (
-		<>
-			<tr>
-				<td>
-					<AvatarImg avatar={user.avatar} />
-				</td>
-				<td>{user.lastName + ' ' + user.firstName}</td>
-				<td>
-					<NavLink to={`/post/${props.slug}`}>{props.title}</NavLink>
-				</td>
-				<td>{category.text}</td>
-				<td>{props.author}</td>
-				<td>
-					<span>
-						{new Date(props.createdAt).toLocaleDateString('en-GB')}
-					</span>
-				</td>
-				<td>
-					<button
-						className="btn--accept"
-						onClick={() => setShowDialog(true)}
-					>
-						Phê duyệt
-					</button>
-				</td>
-				<td>
-					<button
-						className="btn--not-accept"
-						onClick={() => setShowForm(true)}
-					>
-						Từ chối
-					</button>
-				</td>
-			</tr>
-			<Dialog
-				active={showDialog}
-				txtBtnSubmit="Duyệt bài"
-				title="Bạn muốn phê duyệt bài viết này?"
-				styleDialog="primary"
-				onSubmit={handleAccept}
-				onClose={() => setShowDialog(false)}
-			/>
-			<ProtectedComponent dependency={showForm}>
-				<div className="form-send-msg">
-					<ContentEditable
-						html={message}
-						onChange={handleChange}
-						tagName="div"
-						role="textbox"
-						aria-multiline={true}
-						className="input-element"
-						placeholder={'Nhập lý do từ chối'}
-					/>
-					<div className="group-btn">
-						<div
-							className="btn btn--secondary"
-							onClick={() => {
-								setShowForm(false);
-								setMessage('');
-							}}
-						>
-							Hủy
-						</div>
-						<div
-							className="btn btn--primary"
-							onClick={handleNotAccept}
-						>
-							Gửi
-						</div>
-					</div>
-				</div>
-			</ProtectedComponent>
-		</>
-	);
+    return (
+        <>
+            <tr>
+                <td>
+                    <AvatarImg avatar={user.avatar} />
+                </td>
+                <td>{user.lastName + ' ' + user.firstName}</td>
+                <td>
+                    <NavLink to={`/post/${props.slug}`}>{props.title}</NavLink>
+                </td>
+                <td>{category?.text}</td>
+                <td>{props?.author}</td>
+                <td>
+                    <span>
+                        {new Date(props.createdAt).toLocaleDateString('en-GB')}
+                    </span>
+                </td>
+                <td>
+                    <button
+                        className="btn--accept"
+                        onClick={() => setShowDialog(true)}
+                    >
+                        Phê duyệt
+                    </button>
+                </td>
+                <td>
+                    <button
+                        className="btn--not-accept"
+                        onClick={() => setShowForm(true)}
+                    >
+                        Từ chối
+                    </button>
+                </td>
+            </tr>
+            <Dialog
+                active={showDialog}
+                txtBtnSubmit="Duyệt bài"
+                title="Bạn muốn phê duyệt bài viết này?"
+                styleDialog="primary"
+                onSubmit={handleAccept}
+                onClose={() => setShowDialog(false)}
+            />
+            <ProtectedComponent dependency={showForm}>
+                <div className="form-send-msg">
+                    <ContentEditable
+                        html={message}
+                        onChange={handleChange}
+                        tagName="div"
+                        role="textbox"
+                        aria-multiline={true}
+                        className="input-element"
+                        placeholder={'Nhập lý do từ chối'}
+                    />
+                    <div className="group-btn">
+                        <div
+                            className="btn btn--secondary"
+                            onClick={() => {
+                                setShowForm(false);
+                                setMessage('');
+                            }}
+                        >
+                            Hủy
+                        </div>
+                        <div
+                            className="btn btn--primary"
+                            onClick={handleNotAccept}
+                        >
+                            Gửi
+                        </div>
+                    </div>
+                </div>
+            </ProtectedComponent>
+        </>
+    );
 }
 
 export default memo(InfoPost);
